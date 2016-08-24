87d52a9627431c224a4b5e0e745eeafe550ba21d
rwilcox5 <rwilcox5@gmail.com>
2016-08-24 18:19:42 -0400

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,21 @@
 from fabric.api import task
 
+from ftplib import FTP
+
+#domain name or server ip:
+
+
 @task
 def run_bets():
+    ftp = FTP('ftp.byethost24.com')
+    ftp.login(user='b24_18788153', passwd = 'ByeTpi3.14')
+    ftp.cwd('/htdocs/')
+    ftp.retrlines('LIST')
     print 'done in one.'
+    filen = open("newfile.txt", "w")
+    filen.write("hello world in the new filedd")
+    filen.close()
+    filename = "newfile.txt"
+    ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+    ftp.quit()
+    print 'done in tow.'
